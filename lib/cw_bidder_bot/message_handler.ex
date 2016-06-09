2fff4ccf8654631660c6bbb545f15cf7b51c6002
Ilija Eftimov <i.eftimov@catawiki.nl>
2016-06-09 13:04:38 +0200

    Add payload logging to CwBidderBot.MessageHandler.handle/1

--- lib/cw_bidder_bot/message_handler.ex
+++ lib/cw_bidder_bot/message_handler.ex
@@ -33,6 +33,7 @@ defmodule CwBidderBot.MessageHandler do
       }
     }
 
+    Logger.info "Sending payload:\n#{inspect payload}"
     url = "https://graph.facebook.com/v2.6/me/messages?access_token=#{@fb_page_access_token}"
     headers = [{"Content-Type", "application/json"}]
     HTTPoison.post!(url, Poison.encode!(payload), headers)
